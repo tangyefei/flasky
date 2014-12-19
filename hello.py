c97217093b5a820c00fd3323cfe3d093b9a80d79
tangyefei <tyfccsu@gmail.com>
2014-12-19 17:03:28 +0800

    using extension

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
-    from flask import Flask
+from flask import Flask
+from flask.ext.script import Manager
 
-    app = Flask(__name__)
+app = Flask(__name__)
+manager = Manager(app)
 
-    @app.route('/')
-    def index():
-        return '<h1>Hello World!</h1>'
 
-    if __name__ == '__main__':
-        app.run(debug=True)
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
+
+if __name__ == '__main__':
+    manager.run()
