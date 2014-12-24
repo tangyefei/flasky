6073653d2a80bff99588ac2f2fa95a75c3948bad
tangyefei <tyfccsu@gmail.com>
2014-12-24 16:19:09 +0800

    flask-bootstrap using; common error page abstract

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 class Human():
     def somemethod(self):
@@ -49,9 +51,18 @@ def comments():
 def extends():
     return render_template('child.html')
 
+@app.route('/bootstrap/<name>')
+def bootstrap(name):
+    return render_template('bootstrap.html', name=name)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
