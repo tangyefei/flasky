0d4efc0bd06c2f15963e3864077cffac906edacd
tangyefei <tyfccsu@gmail.com>
2014-12-24 11:42:48 +0800

    variables in template

--- index.py
+++ index.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class Human():
+    def somemethod(self):
+        return 'what the fucking world!'
 
 @app.route('/')
 def index():
@@ -9,7 +12,12 @@ def index():
 
 @app.route('/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    mydict = {"key": "To Be or Not To Be"}
+    mylist = ['it', 'is', 'a', 'problem']
+    myintvar = 0
+    myobj = Human()
+
+    return render_template('user.html', name=name, mydict=mydict, mylist=mylist, myintvar=myintvar, myobj=myobj)
 
 if __name__ == '__main__':
     app.run(debug=True)
