65a06e3b1fb95e5d12a15ae6b7387ed8c12395ae
tangyefei <tyfccsu@gmail.com>
2014-12-26 14:39:06 +0800

    db operation in view fucntion

--- index.py
+++ index.py
@@ -11,7 +11,6 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 from flask.ext.script import Shell
 
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
@@ -50,19 +49,26 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-from flask import Flask, render_template, session, redirect, url_for, flash
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+
         session['name'] = form.name.data
         form.name.data = ''
+
         return redirect(url_for('index'))
-    return render_template('index.html',form = form, name = session.get('name'))
+    return render_template('index.html',
+        form = form, name = session.get('name'), known = session.get('known', False))
+
 
 
 @app.route('/<name>')
