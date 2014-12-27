50126b750619303b7cbe84d64aa5d33eea623789
tangyefei <tyfccsu@gmail.com>
2014-12-27 11:58:32 +0800

    mail charpter

--- index.py
+++ index.py
@@ -11,7 +11,9 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
-
+from flask.ext.mail import Mail
+from flask.ext.mail import Message
+from threading import Thread
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -20,13 +22,45 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] =\
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <tyfccsu@gmail.com>'
+
+# def send_email(to, subject, template, **kwargs):
+#     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+#                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+#     msg.body = render_template(template + '.txt', **kwargs)
+#     msg.html = render_template(template + '.html', **kwargs)
+#     mail.send(msg)
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+return thr
+
 db = SQLAlchemy(app)
 manager = Manager(app)
 moment = Moment(app)
 bootstrap = Bootstrap(app)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
-
+mail = Mail(app)
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
@@ -55,15 +89,23 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
+
+
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
-            user = User(username = form.name.data)
+            user = User(username=form.name.data)
             db.session.add(user)
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User',
+                           'mail/new_user', user=user)
         else:
             session['known'] = True
 
@@ -71,8 +113,26 @@ def index():
         form.name.data = ''
 
         return redirect(url_for('index'))
-    return render_template('index.html',
-        form = form, name = session.get('name'), known = session.get('known', False))
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     form = NameForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(username=form.name.data).first()
+#         if user is None:
+#             user = User(username = form.name.data)
+#             db.session.add(user)
+#             session['known'] = False
+#         else:
+#             session['known'] = True
+
+#         session['name'] = form.name.data
+#         form.name.data = ''
+
+#         return redirect(url_for('index'))
+#     return render_template('index.html',
+#         form = form, name = session.get('name'), known = session.get('known', False))
 
 
 
